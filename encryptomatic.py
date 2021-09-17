c0782fb78a4e2bdf14dc617c4661b2cd303415c3
MarkGhebrial <45022215+MarkGhebrial@users.noreply.github.com>
2021-09-17 15:46:51 +0000

    More documentation

--- encryptomatic.py
+++ encryptomatic.py
@@ -4,6 +4,7 @@ class EncryptOMatic:
   def __init__ (self, key: str):
     self.key = key.lower()
 
+    # Create a sequence of three characters to use instead of spaces
     if self.key != "space":
       self.spaceSequence = EncryptOMatic("space").encryptString(key)[:3]
     else:
@@ -11,36 +12,41 @@ class EncryptOMatic:
 
   def encryptChar (self, char: str, position: str):
     try:
-      # Magic
+      # Get the value of the character, then add the value of the character at key[position] and turn it back into a character
       return tebahpla[(alphabet[char.lower()] + alphabet[self.key[position % len(self.key)]]) % len(alphabet)]
     except KeyError:
+      # If the character is not aphanumeric, then don't change it
       if char == " ":
-        return self.spaceSequence#[position % len(self.spaceSequence)]
+        return self.spaceSequence
       return char
 
-  def encryptString (self, data: str):
+  def encryptString (self, data: str) -> str:
+    '''Encrypt each character in data, then retun that value
+    '''
     temp = ""
     for i in range(len(data)):
       temp += self.encryptChar(char=data[i], position=i)
     return temp
 
-  def removeSpaceSequences (self, data: str):
+  def removeSpaceSequences (self, data: str) -> str:
+    '''Recursively replace all space sequences with actual spaces
+    '''
     temp = data[:]
 
     for i in range(len(data) - 2):
-      if data[i:(i+3)] == self.spaceSequence:
-        temp = data[:i] + " " + data[(i+3):]
-        return self.removeSpaceSequences(temp)
+      if data[i:(i+3)] == self.spaceSequence: # Scan next few chars to see if they are the space sequence
+        temp = data[:i] + " " + data[(i+3):] # If so, remove them and put a space instead
+        return self.removeSpaceSequences(temp) # Repeat until there are no more space sequences
     
     return temp
 
-  def decryptChar (self, char: str, position: str):
+  def decryptChar (self, char: str, position: str) -> str:
     try:
       return tebahpla[(alphabet[char.lower()] - alphabet[self.key[position % len(self.key)]]) % len(alphabet)]
     except KeyError:
       return char
 
-  def decryptString (self, data: str):
+  def decryptString (self, data: str) -> str:
     data = self.removeSpaceSequences(data)
 
     temp = ""
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from encryptomatic import EncryptOMatic
 
 e1 = EncryptOMatic(key='212fdjkslkjhuioiwen5432fdsa3456789')
-print(e1.encryptString("                                    "))
+s = e1.encryptString("Most binary Wine packages will associate Wine with .exe files for you. If that is the case, you should be able to simply double-click on the .exe file in your file manager, just like in Windows. You can also right-click on the file, choose \"Run with\", and choose \"Wine\". This may sometimes open the file in the wrong program - if this happens, check the filetype associations for the file using whatever tool your desktop environment provides and edit as needed. Note that if you have built Wine from source you will have to create the filetype association yourself.")
+print(e1.decryptString(s))
