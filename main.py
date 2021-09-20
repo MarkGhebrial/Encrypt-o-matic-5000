e8a4e145ccb5765eb93186138a8264a8a4ef1b88
MarkGhebrial <45022215+MarkGhebrial@users.noreply.github.com>
2021-09-20 23:18:09 +0000

    Add gitignore

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from gui import GUI
 from tkinter import *
 from tkinter import ttk
 
+print("This app uses a GUI made with tk")
+print("If replit decides not to render it, please download an run the app")
+print("A vanilla python 3 installation is all that is needed, no other dependencies")
+
 root = Tk()
 root.title("Encrypt-o-Matic 5000")
 GUI(root)
